670a0dd71919170e791fecf4bd8c66843845a51d
mbarcon7 <marcelo.barcon@patelhs.org>
2020-10-19 18:59:14 +0000

    I think this is what you mean?

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+class Main
+{
+  public static void main(String[] args)
+  {
+    
+    int myAge = 15;
+    int myBirthday = 711;
+    int todaysDate = 921;
+    
+    if(myBirthday == todaysDate)
+    {
+      System.out.print("It is my birthday.");
+       myAge++;
+    System.out.print(" I am now " + myAge + " years old.");  
+    }else{
+      System.out.print("It isn't my birthday.");
+   
+    System.out.println(" I am still " + myAge + " years old.");
+    }
   }
 }
